9a0d0962a0c2f4c45184ef4aca89ed0a6770c36e
wayandarma <wayandarma377@gmail.com>
2026-03-14 01:12:33 +0800

    feat: replace manual student routes with Route::resource()

--- laravel-learn/routes/web.php
+++ laravel-learn/routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/hello', [HelloController::class, 'hello']);
 
+Route::resource('students', StudentController::class);
