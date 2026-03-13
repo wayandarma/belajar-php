33e460f0cd1d8fabf483832ae6efcfd613a6ca45
wayandarma <wayandarma377@gmail.com>
2026-03-14 00:37:20 +0800

    feat: add basic 'Hello World' page with a new route, controller, and view.

--- laravel-learn/routes/web.php
+++ laravel-learn/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/hello', [HelloController::class, 'hello']);
+
